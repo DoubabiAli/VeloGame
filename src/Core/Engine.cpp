c869f4c604a69c06ca33f5d33aa31a665ff83420
DoubabiAli <doubabiali@gmail.com>
2025-04-13 14:45:28 +0100

    Version 1.0.0 (fenetre)

---
+++ src/Core/Engine.cpp
@@ -0,0 +1,55 @@
+#include "Engine.h"
+#include <iostream>
+
+Engine* Engine::s_Instance = nullptr;
+
+bool Engine::Init()
+{
+    if (SDL_Init(SDL_INIT_VIDEO)!= 0 && IMG_Init(IMG_INIT_JPG | IMG_INIT_PNG) != 0)
+    {
+        SDL_Log("Failed to initialize SDL: %s", SDL_GetError());
+        return false;
+    }
+
+    m_Window = SDL_CreateWindow("Velo Game", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_HEIGHT, SCREEN_WIDTH, 0);
+    if(m_Window == nullptr)
+    {
+       SDL_Log("Failed to create Window: %s", SDL_GetError());
+       return false;
+    }
+
+    m_Renderer = SDL_CreateRenderer(m_Window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+    if(m_Renderer == nullptr)
+    {
+        SDL_Log("Failed to create Renderer: %s", SDL_GetError());
+        return false;
+    }
+
+    return m_IsRunning = true;
+}
+
+void Engine::Update()
+{
+    SDL_Log("Running");
+}
+
+void Engine::Render()
+{
+    SDL_SetRenderDrawColor(m_Renderer, 255, 255, 255, 255);
+    SDL_RenderPresent(m_Renderer);
+}
+
+void Engine::Events()
+{
+
+}
+
+bool Engine::Clean()
+{
+
+}
+
+void Engine::Quit()
+{
+
+}
